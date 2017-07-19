73563f8dcf3705a774d4c72c6528f0b1f1077caf
winnie0521 <wenyue.xing@brown.edu>
2017-07-19 15:24:21 -0400

    moving test to test folders

--- tests/sequence_content_test.R
+++ tests/sequence_content_test.R
@@ -1,8 +1,7 @@
-test_that("test sequence content",{
-  expect_equal(sequence_content("Data/10^5_reads_test.fq.gz","N"),manual_sequence_content("Data/10^5_reads_test.fq.gz","N"))
-  expect_equal(sequence_content("Data/10^5_reads_test.fq.gz","A"),manual_sequence_content("Data/10^5_reads_test.fq.gz","A"))
-  expect_equal(sequence_content("Data/10^5_reads_test.fq.gz","C"),manual_sequence_content("Data/10^5_reads_test.fq.gz","C"))
-  expect_equal(sequence_content("Data/10^5_reads_test.fq.gz","T"),manual_sequence_content("Data/10^5_reads_test.fq.gz","T"))
-  expect_equal(sequence_content("Data/10^5_reads_test.fq.gz","G"),manual_sequence_content("Data/10^5_reads_test.fq.gz","G"))
+test_that("Test sequence content",{
+  expect_equal( sequence_content("Data/10^5_reads_test.fq.gz","g"),manual_sequence_content("Data/10^5_reads_test.fq.gz","g"))
+  expect_equal( sequence_content("Data/10^5_reads_test.fq.gz","c"),manual_sequence_content("Data/10^5_reads_test.fq.gz","c"))
+  expect_equal( sequence_content("Data/10^5_reads_test.fq.gz","a"),manual_sequence_content("Data/10^5_reads_test.fq.gz","a"))
+  expect_equal( sequence_content("Data/10^5_reads_test.fq.gz","t"),manual_sequence_content("Data/10^5_reads_test.fq.gz","t"))
+  expect_equal( sequence_content("Data/10^5_reads_test.fq.gz","n"),manual_sequence_content("Data/10^5_reads_test.fq.gz","n"))
 })
-
