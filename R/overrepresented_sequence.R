49e5918e7bf7bc7b66b126b28d4b73ffd5a9a2d1
winnie0521 <wenyue.xing@brown.edu>
2017-07-25 15:57:48 -0400

    Overrepresented sequence write to txt

--- R/overrepresented_sequence.R
+++ R/overrepresented_sequence.R
@@ -3,7 +3,7 @@ overrepresented_sequence <- function(name){
   default <- "gunzip -c defaultname | awk 'NR%4==2' | sort |uniq -c | sort -r > over_rep_reads.txt "
   command <- sub("defaultname",name,default)
   system(command,intern = TRUE)
-  over_rep <- read.table("over_rep_reads.txt",sep="/n")
-  over_rep_table <- as.data.frame(over_rep %>% dplyr::group_by(a)%>%summarize(count=n())%>%arrange(desc(count)))
+  over_rep <- read.table("over_rep_reads.txt")
+  over_rep_table <- as.data.frame(over_rep%>%arrange(desc(V1)))
   return(over_rep_table)
 }
