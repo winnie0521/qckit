22bb3eef4cf9bb73018b946b64f86118b47b75ad
winnie0521 <wenyue.xing@brown.edu>
2017-07-25 15:51:56 -0400

    Overrepresented sequence write to txt

--- R/manual_mean.R
+++ R/manual_mean.R
@@ -4,7 +4,9 @@
   #read the data ShortRead
   fastq <- ShortRead::readFastq(file_name)
   ## as a quality score matrix
+
   scorecycle<-as(Biostrings::quality(fastq), "matrix")
   quality_mean_sr<-as.vector(colMeans(as.data.frame(scorecycle)))
   return(quality_mean_sr)
 }
+
--- R/overrepresented_sequence.R
+++ R/overrepresented_sequence.R
@@ -1,8 +1,9 @@
 overrepresented_sequence <- function(name){
   # use system to extract unique counts and count them
-  default <- "gunzip -c defaultname | awk 'NR%4==2' | sort |uniq -c | sort -r "
+  default <- "gunzip -c defaultname | awk 'NR%4==2' | sort |uniq -c | sort -r > over_rep_reads.txt "
   command <- sub("defaultname",name,default)
-  a<-system(command,intern = TRUE)
-  over_rep <- as.data.frame(a)
-  return(over_rep)
+  system(command,intern = TRUE)
+  over_rep <- read.table("over_rep_reads.txt",sep="/n")
+  over_rep_table <- as.data.frame(over_rep %>% dplyr::group_by(a)%>%summarize(count=n())%>%arrange(desc(count)))
+  return(over_rep_table)
 }
