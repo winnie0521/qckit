4a2bbc77db47bbaaf7d9fb438ca0149d31d73700
winnie0521 <wenyue.xing@brown.edu>
2017-09-05 13:41:31 -0400

    change GC content plot

--- R/GC_content_plot.R
+++ R/GC_content_plot.R
@@ -17,17 +17,15 @@ GC_content <- function(nc){
   gc_list <-sapply(gc_result$gc_table,function(x) {table(unlist(strsplit(x,'')))})
   gc_percent <- sapply(gc_list[1:nc], function(x)
   {
-    y<-c(0,0,0,0)
+    y<-c(0,0,0)
     if ( "G" %in% names(x)){ y[1] <- x[names(x)=="G"]}
     if ("C" %in% names(x)){y[2] <-x[names(x)=="C"]}
-    y[3] <-sum(x)
-    y[4] = y[1]+y[2]
+    y[3] = y[1]+y[2]
     return(y)
 
   })
 
-  gc <- gc_percent
-  gc_df <- as.data.frame(gc[4,])
+  gc_df <- as.data.frame(gc_percent[4,])
   colnames(gc_df) = c("meanGC")
 
   p1 <- ggplot2::ggplot(data=gc, ggplot2::aes(meanGC)) +ggplot2::geom_histogram(breaks=seq(0, ncol(contentcycle), by=1))
