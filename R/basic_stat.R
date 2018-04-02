d5992894753c5763efaa4b76d528e847420b8b94
winnie0521 <wenyue.xing@brown.edu>
2018-04-02 19:47:33 -0400

    basic_stat

--- R/basic_stat.R
+++ R/basic_stat.R
@@ -1,17 +1,15 @@
 #' Generate the data frame that includes percentiles of quality score per position
 #' @param infile the object that is the dataframe of the mean, median and quantiles of the FASTQ file from basic statistics function
-#' @return boxplot of per position quality score distribution
+#' @param writefile the boolean of whether to write output as file
+#' @param prefix the string of customized prefix of write-out file
+#' @return table of quality score statistics
 #' @export
 #'
 #' @author Wenyue Xing, \email{wenyue_xing@@brown.edu}
 
-basic_stat <- function(infile){
+basic_stat <- function(infile,writefile=FALSE,prefix){
 
   basic_stat <- qual_score_per_read(infile)
-  basic_statistics <- data.frame(q01 = basic_stat$q01_per_position,
-                                 q25 = basic_stat$q25_per_position,
-                                 median = basic_stat$q50_per_position,
-                                 q75 = basic_stat$q75_per_position,
-                                 q99 = basic_stat$q99_per_position)
-  return(basic_statistics)
+  if (writefile==TRUE) write.csv(file=paste0(prefix,"basicstat.csv"),basic_stat)
+  return(basic_stat)
 }
